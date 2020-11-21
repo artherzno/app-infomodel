594727fa8e44b968443f7fadcb1a95132b9416f6
Artherzno <zkyzno@Artherznos-MacBook-Pro.local>
2020-11-21 14:21:53 +0700

    - Add react navigation

--- App.js
+++ App.js
@@ -1,63 +1,66 @@
-import React, { Component } from 'react';
-import { View, Text, FlatList, SafeAreaView, StyleSheet, Button, TouchableOpacity} from 'react-native';
+import * as React from 'react';
+import { Button, View, Text, TouchableHighlight} from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import modelList from './data/datalist.json';
+function NavModel() {
+  
+  const navigation = useNavigation();
 
-export default function App() {
-  const itemModel = ({item})=>(
-    <View>
-      <Text style={styles.p}>{item.model}</Text>
-    </View>
-  );
-
-  const groupModel = ({item})=>(
-    <View style={styles.table}>
-      <Text style={styles.p}>{item.type}</Text>
-      <FlatList 
-        data={item.listmodel}
-        renderItem={itemModel}
-        keyExtractor={item=> item.id}
-      />
-    </View>
-
-  );
-
+  const button = {
+    padding: 20,
+    backgroundColor: '#00aa00'
+  };
+  
   return (
-    <SafeAreaView style={ styles.container }> 
-      <Text style={ styles.h1 }>
-        Kawasaki Info Model
-      </Text>
-      <FlatList 
-        data={modelList} 
-        renderItem={groupModel} 
-        keyExtractor={item=>item.id}
-        horizontal={true}
-      />
-    </SafeAreaView>
+    <View>
+      <TouchableHighlight style={button} onPress={()=>navigation.navigate('Home')}>
+        <Text>Home</Text>
+      </TouchableHighlight>
+      <TouchableHighlight style={button} onPress={()=>navigation.navigate('Overview')}>
+        <Text>Overview</Text>
+      </TouchableHighlight>
+    </View>
+    
+
   );
-  }
+}
 
-const colorGreen = 'green';
-const colorWhite = 'white';
+function HomeScreen({ navigation }) {
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#000',
-    flex: 1,
-    alignItems: 'center',
-  },
-  h1: {
-    color: colorGreen,
-    fontSize: 40,
-  },
-  p: {
-    color: '#fff',
-    fontSize: 16,
-  },
-  table: {
-    padding: 10,
-    backgroundColor: '#0a0a0a',
-    borderWidth: 1,
-    borderColor: colorGreen,
-  }
-});
+  // console.log({navigation});
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <NavModel />
+    </View>
+  );
+}
+
+function OverviewScreen({ navigation }) {
+  // console.log({navigation});
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Overview</Text>
+      <NavModel />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+          headerShown: false
+        }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Overview" component={OverviewScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
